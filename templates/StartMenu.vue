db994d90a1ca104b90c144a1073e504b1e29d10d
lizardon <donchia@ymail.com>
2024-09-28 00:53:57 +0800

    Update StartMenu.vue

--- templates/StartMenu.vue
+++ templates/StartMenu.vue
@@ -4,7 +4,7 @@
         <img class="sidebar-image" src="@/assets/sidebar-image.png" />
     </div>
     <div class="socials">
-        <a href="https://www.instagram.com/_dontax_/" target="_blank">
+        <a href="https://www.instagram.com/lizardonthecamera/" target="_blank">
             <div class="bar">
                 <img class="social-image" src="@/assets/iPhone-Icons/Instagram.webp" />
                 <div class="social-text"><u>I</u>nstagram</div>
